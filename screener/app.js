6efcb8ad5c3cc063075e8b02878d675fa133157c
Jerin Kurian <jerin7931@gmail.com>
2026-09-21 19:23:36 -0500

    Version workstation assets for deployment

--- screener/app.js
+++ screener/app.js
@@ -1,7 +1,7 @@
 import {CONFIG} from "../config.js";
-import {buildDemoData} from "./demo-data.js";
-import {availableIndustries,defaultFilters,normalizedLive,parseRoute,selectOpportunity} from "./dashboard-core.js";
-import {renderWorkstation,renderDetail} from "./workstation-view.js";
+import {buildDemoData} from "./demo-data.js?v=3.0.3";
+import {availableIndustries,defaultFilters,normalizedLive,parseRoute,selectOpportunity} from "./dashboard-core.js?v=3.0.3";
+import {renderWorkstation,renderDetail} from "./workstation-view.js?v=3.0.3";
 
 const $=id=>document.getElementById(id);
 const client=window.supabase.createClient(CONFIG.supabaseUrl,CONFIG.supabasePublishableKey,{auth:{persistSession:true,autoRefreshToken:true,detectSessionInUrl:true}});
